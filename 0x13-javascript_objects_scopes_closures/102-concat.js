c5ef35040b42345f670cfe9c330b8ac1c60d468f
thinktank01 <felixayot@gmail.com>
2023-09-12 23:50:43 +0300

    Include the correct file system module i.e (fs) from Node.js API to enable reading and writing of files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fileSource = require('fileSource');
-const f1 = fileSource.readFileSync(process.argv[2], 'utf8');
-const f2 = fileSource.readFileSync(process.argv[3], 'utf8');
-fileSource.writeFileSync(process.argv[4], f1 + f2);
+const fileSystem = require('fs');
+const f1 = fileSystem.readFileSync(process.argv[2], 'utf8');
+const f2 = fileSystem.readFileSync(process.argv[3], 'utf8');
+fileSystem.writeFileSync(process.argv[4], f1 + f2);
