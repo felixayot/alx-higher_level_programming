70f2e45df2dbec31b4617eeb31349041de399a99
thinktank01 <felixayot@gmail.com>
2023-09-18 14:06:36 +0300

    Fix table alias mismatches in code

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT `cur`.`id`, `cur`.`name`, `s`.`name` \
+    cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                 FROM `cities` as `c` \
                 JOIN `states` as `s` \
                     ON `c`.`state_id` = `s`.`id` \
