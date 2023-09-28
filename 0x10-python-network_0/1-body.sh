44652e4d0988135d76b0b4beffcd38b5613fa34d
thinktank01 <felixayot@gmail.com>
2023-09-28 17:33:09 +0300

    Fix line count to 3 in bash scripts

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL,
-#  and displays the body of the response.
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response.
 curl -s -L "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL as an argument, sends a GET request to the URL,
-#  and displays the body of the response.
+# Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
 curl -s -H "X-School-User-Id: 98" "$1"
