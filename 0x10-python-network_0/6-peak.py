5ef079508b028418a2cfb7ec1e7f0b2e800f9ee8
thinktank01 <felixayot@gmail.com>
2023-09-28 18:39:34 +0300

    All right, let's try a more efficient way of handling an empty list.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,10 +7,12 @@ def find_peak(list_of_integers):
     Args:
      list_of_integers(list): list of unsorted ints to find the peak of.
     """
-    # Keep track of highest value integer(peak).
-    peak = None
-    # Iterate through the list while keeping track of the peak.
-    for element in list_of_integers:
-        if peak is None or peak < element:
-            peak = element
-    return peak
+    if list_of_integers:
+        # Keep track of highest value integer(peak).
+        peak = 0
+        # Iterate through the list while keeping track of the peak.
+        for element in list_of_integers:
+            if peak < element:
+                peak = element
+        return peak
+    return None
