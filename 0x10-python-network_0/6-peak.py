f4ed98f94ee15b9d18d6e244e1007dd59b9a5f79
thinktank01 <felixayot@gmail.com>
2023-09-28 18:16:53 +0300

    Change algorithm to loop through the list and keep track of the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,22 +7,10 @@ def find_peak(list_of_integers):
     Args:
      list_of_integers(list): list of unsorted ints to find the peak of.
     """
-    if list_of_integers != []:
-        size = len(list_of_integers)
-        if size == 1:
-            return list_of_integers[0]
-        elif size == 2:
-            return max(list_of_integers)
-
-        midindex = int(size / 2)
-        peak = list_of_integers[midindex]
-        if peak > list_of_integers[midindex - 1] \
-                and peak > list_of_integers[midindex + 1]:
-            return peak
-        elif peak < list_of_integers[midindex - 1] \
-                and peak < list_of_integers[midindex + 1]:
-            return find_peak(list_of_integers[:midindex])
-        else:
-            return find_peak(list_of_integers[midindex:])
-
-    return None
+    # Keep track of highest value integer(peak).
+    peak = None
+    # Iterate through the list while keeping track of the peak.
+    for element in list_of_integers:
+        if peak is None or peak < element:
+            peak = element
+    return peak
