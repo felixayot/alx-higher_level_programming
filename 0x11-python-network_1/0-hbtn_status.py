06bd686fde32ee5fb4e14c837a1f953064854b39
thinktank01 <felixayot@gmail.com>
2023-09-29 16:42:17 +0300

    Use format strings in the output formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         page = response.read()
         print("Body response:")
-        print("\t- type: ", type(page))
-        print("\t- content: ", page)
-        print("\t- utf8 content: ", page.decode("UTF-8"))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("UTF-8")))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     page = response.text
     print("Body response:")
-    print("\t- type: ", type(page))
-    print("\t- content: ", page)
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
